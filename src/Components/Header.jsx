6e443bc9cda6a1d01d1f0ff2ff568b023821aed6
MsadafK <mssadaf0@gmail.com>
2025-02-11 12:24:10 +0530

    Deploy

--- src/Components/Header.jsx
+++
@@ -1,9 +0,0 @@
-import PropTypes from "prop-types";
-
-export default function Header({ children }) {
-  return <header>{children}</header>;
-}
-
-Header.propTypes = {
-  children: PropTypes.node.isRequired,
-};
--- src/Components/Main.jsx
+++
@@ -1,9 +0,0 @@
-import PropTypes from "prop-types";
-
-export default function Main({ children }) {
-  return <main className="font-karla">{children}</main>;
-}
-
-Main.propTypes = {
-  children: PropTypes.node.isRequired,
-};
